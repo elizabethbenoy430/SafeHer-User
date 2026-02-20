e63b9874d3c6693e1794a274498f921b35a0f00c
elizabethbenoy430 <elizabethbenoy430@gmail.com>
2026-02-20 10:27:30 +0530

    MAIN DART

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:user_app/editprofile.dart';
-import 'package:user_app/homepage.dart';
 import 'package:user_app/login.dart';
-import 'package:user_app/myprofile.dart';
-import 'package:user_app/userregistration.dart';
+
 
 
 Future<void> main() async {
